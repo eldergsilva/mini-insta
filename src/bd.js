a7dc9b62df12218e22c3fb58a1bc0ddac3bffd3c
eldergsilva <eldergsilva@gmail.com>
2026-07-01 11:44:38 -0300

    ajustadas as configuracoes para dotenv

--- src/bd.js
+++ src/bd.js
@@ -1,10 +1,11 @@
+dotenv.config()
 const knex= require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: '1234',
-        database: 'mini_insta'
+        host:process.env.DB_HOST, 
+        user:process.env.DB_USER,   
+        password:process.env.DB_PASSWORD,  
+        database:process.env.DB_NAME
     }
 })       
-modle.exports= knex
+module.exports= knex
