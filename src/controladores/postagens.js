507c9eb4d383aac4aa7b1e6f39afc017f29968e0
eldergsilva <eldergsilva@gmail.com>
2026-07-11 10:54:31 -0300

    ajustes de validacoes

--- src/controladores/postagens.js
+++ src/controladores/postagens.js
@@ -3,93 +3,96 @@ const knex = require('../bd');
 const novaPostagem = async (req, res) => {
     const { id } = req.usuario;
     const { texto, fotos } = req.body;
-    
-    if (fotos && fotos.length === 0) {
+
+    if (!fotos || fotos.length === 0) {
         return res.status(400).json({ mensagem: 'É necessário enviar ao menos uma foto' });
     }
 
     try {
         const postagem = await knex('postagem').insert({
             usuario_id: id,
-            texto            
+            texto
         }).returning('*');
 
-        if(!postagem) {
+        if (!postagem) {
             return res.status(400).json({ mensagem: 'Não foi possível concluir a postagem' });
         }
 
         for (const foto of fotos) {
             foto.postagem_id = postagem[0].id;
         }
-         
+
         const fotosCadastradas = await knex('postagem_foto').insert(fotos).returning('*');
 
-        if(!fotosCadastradas){
+        if (!fotosCadastradas) {
             await knex('postagem').where({ id: postagem[0].id }).del();
             return res.status(400).json({ mensagem: 'Não foi possível cadastrar as fotos da postagem' });
         }
+
         return res.status(200).json({ mensagem: 'Postagem criada com sucesso' });
 
-    }catch (error) {
-        return res.status(500).json(error.message);
+    } catch (error) {
+        return res.status(500).json({ mensagem: error.message });
     }
-
-    
 };
 
-const curtir = async (req, res) => {    
-    const {id} = req.usuario;
-    const {postagemId} = req.params;
+const curtir = async (req, res) => {
+    const { id } = req.usuario;
+    const { postagemId } = req.params;
 
     try {
-        const postagem = await knex('postagem').where({id: postagemId}).first();
-        if(!postagem){
-            return res.status(404).json({mensagem: 'Postagem não encontrada'});
+        const postagem = await knex('postagem').where({ id: postagemId }).first();
+        if (!postagem) {
+            return res.status(404).json({ mensagem: 'Postagem não encontrada' });
         }
-        
-        const JaCurtiu = await knex('postagem_curtidas').where({usuario_id: id, postagem_id: postagemId}).first();
-        if(JaCurtiu){
-            return res.status(400).json({mensagem: 'Você já curtiu essa postagem'});
+
+        const jaCurtiu = await knex('postagem_curtidas').where({ usuario_id: id, postagem_id: postagemId }).first();
+        if (jaCurtiu) {
+            return res.status(400).json({ mensagem: 'Você já curtiu essa postagem' });
         }
-       const curtida = await knex('postagem_curtidas').insert({usuario_id: id, postagem_id: postagemId});
 
-       if( !curtida){
-        return res.status(400).json({mensagem: 'Não foi possível curtir a postagem'});
-       }
-       
-       return res.status(200).json({mensagem: 'Postagem curtida com sucesso'});
-    }catch (error) {
-        return res.status(500).json(error.message);
-    }    
-}    
+        const curtida = await knex('postagem_curtidas').insert({ usuario_id: id, postagem_id: postagemId }).returning('*');
 
+        if (!curtida) {
+            return res.status(400).json({ mensagem: 'Não foi possível curtir a postagem' });
+        }
 
-const comentar = async (req, res) => {    
-    const {id} = req.usuario;
-    const {postagemId} = req.params;
-    const {texto} = req.body;
+        return res.status(200).json({ mensagem: 'Postagem curtida com sucesso' });
+    } catch (error) {
+        return res.status(500).json({ mensagem: error.message });
+    }
+};
 
-    if(!texto){
-        return res.status(404).json({mensagem: 'Para comentar nessa postagem é necessario  informar o texto'});
+const comentar = async (req, res) => {
+    const { id } = req.usuario;
+    const { postagemId } = req.params;
+    const { texto } = req.body;
+
+    if (!texto) {
+        return res.status(400).json({ mensagem: 'Para comentar nessa postagem é necessário informar o texto' });
     }
 
     try {
-        const postagem = await knex('postagem').where({id: postagemId}).first();
-        if(!postagem){
-            return res.status(404).json({mensagem: 'Postagem não encontrada'});
+        const postagem = await knex('postagem').where({ id: postagemId }).first();
+        if (!postagem) {
+            return res.status(404).json({ mensagem: 'Postagem não encontrada' });
         }
-        const comentario = await knex('postagem_comentarios').insert({usuario_id: id, postagem_id: postagemId, texto: texto}).returning('*');
 
-        if(!comentario){
-            return res.status(400).json({mensagem: 'Não foi possível comentar a postagem'});
+        const comentario = await knex('postagem_comentarios').insert({
+            usuario_id: id,
+            postagem_id: postagemId,
+            texto: texto
+        }).returning('*');
+
+        if (!comentario) {
+            return res.status(400).json({ mensagem: 'Não foi possível comentar a postagem' });
         }
-        
 
-       return res.status(200).json({mensagem: 'Postagem comentada com sucesso'});
-    }catch (error) {
-        return res.status(500).json(error.message);
-    }    
-}
+        return res.status(200).json({ mensagem: 'Postagem comentada com sucesso' });
+    } catch (error) {
+        return res.status(500).json({ mensagem: error.message });
+    }
+};
 
 const feed = async (req, res) => {
     const { id } = req.usuario;
@@ -133,12 +136,11 @@ const feed = async (req, res) => {
     } catch (error) {
         return res.status(500).json({ mensagem: error.message });
     }
-}
+};
 
 module.exports = {
     novaPostagem,
     curtir,
     comentar,
     feed
-}
- 
+};
